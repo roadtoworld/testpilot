f44aa7b18f823f700f1ab80e03f237a1f0408f4a
Apple <apple@Apples-Air-2.lan>
2026-03-12 14:26:28 +0400

    Fix config for Railway

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   serverExternalPackages: ['better-sqlite3', 'playwright'],
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
